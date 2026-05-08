04b3aeebe6ff18f66d3babca6bf42797ed746664
Unyc1124 <aditidubey792@gmail.com>
2026-05-08 23:49:20 +0530

    fix: replace related with blog in search blade

--- resources/views/blogs/search.blade.php
+++ resources/views/blogs/search.blade.php
@@ -129,9 +129,7 @@
 
                     <div class="blog-card-image-wrapper">
 
-                        <!-- <img src="{{ asset($blog->featured_image) }}" -->
-                         <img src="{{ $related->featured_image }}">
-                             alt="{{ $blog->title }}">
+                        <img src="{{ $blog->featured_image }}" alt="{{ $blog->title }}">                        
 
                         <span class="blog-category">
 
