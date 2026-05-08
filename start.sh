b532525f038d79f54def4af796efa8e5eec8797c
Unyc1124 <aditidubey792@gmail.com>
2026-05-08 22:53:32 +0530

    ok

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations..."
+echo "Clearing config..."
 php artisan config:clear
 php artisan cache:clear
+
+echo "Running migrations..."
 php artisan migrate --force
 
+echo "Linking storage..."
+php artisan storage:link || true
 
+echo "Optimizing..."
+php artisan optimize
 
 echo "Starting nginx..."
 exec /start.sh
